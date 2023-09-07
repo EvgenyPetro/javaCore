9573b96bc1462566666ebbc7b8be586a10118458
Evgeny Petrov <petroves88@mail.ru>
2023-09-07 14:12:48 +0300

    base lesson2 first

--- lesson2/src/main/java/ru/petrov/Main.java
+++ lesson2/src/main/java/ru/petrov/Main.java
@@ -11,11 +11,12 @@ public class Main {
 
     private static final int fieldX = 4;
     private static final int fieldY = 4;
+    private static final int winnerCombination = 2;
 
     private static final Scanner scanner = new Scanner(System.in);
     private static final Random rand = new Random();
 
-    private static final char[][] field = new char[fieldX][fieldY];
+    private static char[][] field;
 
     public static void main(String[] args) {
         init();
@@ -23,18 +24,19 @@ public class Main {
         while (true) {
             humanTurn();
             printField(field);
-            if (checkWin(USER_PIC)){
-                return;
+            if (checkWinnerCombination(USER_PIC, "User WINNER")) {
+                break;
             }
             AITurn();
             printField(field);
-            if (checkWin(USER_PIC)){
-                return;
+            if (checkWinnerCombination(AI_PIC, "AI WINNER")) {
+                break;
             }
         }
     }
 
     private static void init() {
+        field = new char[fieldX][fieldY];
 
         for (int x = 0; x < fieldX; x++) {
             for (int y = 0; y < fieldY; y++) {
@@ -68,11 +70,41 @@ public class Main {
     }
 
     private static void humanTurn() {
-        System.out.printf("Введите координаты хода X и Y(от 1 до %d)\n", fieldX);
-        System.out.println(">>>");
-        int x = scanner.nextInt() - 1;
-        int y = scanner.nextInt() - 1;
+        int x, y;
+
+        do {
+
+            while (true) {
+                System.out.printf("Введите координаты хода X (от 1 до %d)\n", fieldX);
+                if (scanner.hasNextInt()) {
+                    x = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    break;
+                } else {
+                    System.out.println("Некорректное число, повторите попытку ввода.");
+                    scanner.nextLine();
+                }
+            }
+
+            while (true) {
+                System.out.printf("Введите координаты хода Y(от 1 до %d)\n", fieldY);
+                if (scanner.hasNextInt()) {
+                    y = scanner.nextInt() - 1;
+                    scanner.nextLine();
+                    break;
+                } else {
+                    System.out.println("Некорректное число, повторите попытку ввода.");
+                    scanner.nextLine();
+                }
+            }
+        }
+        while (!isCellValid(x, y) || !isCellEmpty(x, y));
         field[x][y] = USER_PIC;
+
+    }
+
+    private static boolean isCellEmpty(int x, int y) {
+        return field[x][y] == EMPTY_PIC;
     }
 
     private static void AITurn() {
@@ -93,11 +125,55 @@ public class Main {
 
     }
 
-    private static boolean checkWin(char c) {
-        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) {
-            System.out.println("Winner " + c);
+    private static boolean isCellValid(int x, int y) {
+        if (x < 0 || x >= fieldX || y < 0 || y >= fieldY) {
+            return false;
+        }
+        return field[x][y] == EMPTY_PIC;
+    }
+
+    private static boolean checkWinnerCombination(char c, String s) {
+        if (checkWin(c)) {
+            System.out.println(s);
             return true;
         }
+        if (checkDraw()) {
+            System.out.println("Ничья!");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static boolean checkDraw() {
+        for (int x = 0; x < fieldX; x++) {
+            for (int y = 0; y < fieldY; y++) {
+                if (isCellEmpty(x, y)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
+
+    private static boolean checkWin(char c){
+
+        // Проверка по трем горизонталям
+        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
+        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+
+        // Проверка по трем вертикалям
+        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
+        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
+        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+
+        // Проверка по диагоналям
+        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
+        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+
         return false;
     }
 }
