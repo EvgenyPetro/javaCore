836f96e3b183af8c4a71f3a3c773a959a4b80563
Evgeny Petrov <petroves88@mail.ru>
2023-09-14 16:08:35 +0300

    lesson5 point1

--- lesson5/src/Backup.java
+++ lesson5/src/Backup.java
@@ -1,5 +1,40 @@
+import java.io.*;
+
 public class Backup {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        File source = new File("/Users/user1/geekbrains/java/core/lesson5");
+        File dist = new File("/Users/user1/geekbrains/java/core/backup");
+        copyFilesAndDir(source, dist);
+    }
+
+    public static void copyFilesAndDir(File sourceDir, File destDir) throws IOException {
+
+        destDir.mkdirs();
+        for (String file : sourceDir.list()) {
+            copyDirectory(new File(sourceDir, file), new File(destDir, file));
+        }
+    }
+
+
+    public static void copyDirectory(File source, File dist) throws IOException {
+        if (source.isDirectory()) {
+            copyFilesAndDir(source, dist);
+        } else {
+            copyFile(source, dist);
+        }
 
     }
+
+    private static void copyFile(File sourceFile, File destinationFile)
+            throws IOException {
+        try (InputStream in = new FileInputStream(sourceFile);
+             OutputStream out = new FileOutputStream(destinationFile)) {
+            byte[] buf = new byte[1024];
+            int length;
+            while ((length = in.read(buf)) > 0) {
+                out.write(buf, 0, length);
+            }
+        }
+    }
+
 }
